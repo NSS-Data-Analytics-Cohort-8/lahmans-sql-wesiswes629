11fa337db6b5a002a197e7ee2a7b5a2457e877af
Wes Ward <wes.ward629@gmail.com>
2023-02-28 19:45:29 -0600

    start of 2

--- scripts/lahmans.sql
+++ scripts/lahmans.sql
@@ -7,8 +7,13 @@ FROM teams;
 
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
 
-
+SELECT playerID, namegiven, height
+FROM people
+WHERE height IN (
+SELECT MIN(height)
+FROM people);
    
+-- ANSWER: Edward Carl was 43 inches tall.
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 	
